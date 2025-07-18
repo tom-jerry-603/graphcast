c2568bd84e976de1cf436dc1717cd85c1ad4d25b
tom-jerry-603 <tom.jerry2000603@gmail.com>
2025-07-18 19:15:22 +0900

    change the folder structure

--- graphcast/env.sh
+++
@@ -1,8 +0,0 @@
-sudo apt purge cuda* nvidia-cuda-toolkit -y
-sudo apt autoremove -y
-
-wget https://developer.download.nvidia.com/compute/cuda/12.6.2/local_installers/cuda-repo-ubuntu2204-12-6-local_12.6.2-555.42.02-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2204-12-6-local_12.6.2-555.42.02-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2204-12-6-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo apt update
-sudo apt install cuda -y
--- graphcast/model/download.sh
+++
@@ -1,8 +0,0 @@
-mkdir params
-mkdir stats
-cd params
-wget https://storage.googleapis.com/dm_graphcast/graphcast/params/GraphCast_operational%20-%20ERA5-HRES%201979-2021%20-%20resolution%200.25%20-%20pressure%20levels%2013%20-%20mesh%202to6%20-%20precipitation%20output%20only.npz
-cd ../stats
-wget https://storage.googleapis.com/dm_graphcast/graphcast/stats/stddev_by_level.nc
-wget https://storage.googleapis.com/dm_graphcast/graphcast/stats/mean_by_level.nc
-wget https://storage.googleapis.com/dm_graphcast/graphcast/stats/diffs_stddev_by_level.nc
